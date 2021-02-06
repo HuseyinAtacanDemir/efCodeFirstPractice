63f50f2456964573ea165bbdfecbf4b51edf0afb
HuseyinAtacanDemir <h.atacandemir@gmail.com>
2021-02-06 14:18:15 +0300

    first commit

---
+++ Rocky/Controllers/CategoryController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Rocky.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objList = _db.Category;
+
+            //Console.WriteLine(objList);
+
+            return View(objList);
+        }
+
+        // GET: create
+        public IActionResult Create()
+        {
+            
+            return View();
+        }
+    }
+}
